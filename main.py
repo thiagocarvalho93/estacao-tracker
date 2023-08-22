0da468dd020126a2eb39c2865888c1350edc8ef2
thiagocarvalho93 <thiagopaes1993@gmail.com>
2023-08-21 22:21:59 -0300

    👷 Teste actions

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import csv
+import os
 import requests
 
 API_URL = "https://api.weather.com/v2/pws/observations/current?apiKey={key}&stationId={id}&numericPrecision=decimal&format=json&units=m"
-API_KEY = "e1f10a1e78da46f5b10a1e78da96f525"
+try:
+    API_KEY = os.environ["API_KEY"]
+except KeyError:
+    API_KEY = "e1f10a1e78da46f5b10a1e78da96f525"
 
 estacoes = []
 
