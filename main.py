b33ab9384ccfbfb6852985ca09ff228bf74fdcc4
thiagocarvalho93 <thiagopaes1993@gmail.com>
2023-08-27 10:50:56 -0300

    ♻️ Refatorando para melhor legibilidade

--- main.py
+++ main.py
@@ -7,12 +7,9 @@ import time
 import requests
 
 START_TIME = time.time()
-logging.basicConfig(level=logging.INFO, filename="programa.log",
-                    format="%(asctime)s - %(levelname)s - %(message)s")
-
 API_URL = "https://api.weather.com/v2/pws/observations/current?apiKey={key}&stationId={id}&numericPrecision=decimal&format=json&units=m"
-
 try:
+    # Altere as variáveis abaixo com os valores correspondentes para rodar localmente
     API_KEY = os.environ["API_KEY"]
     SENDER_EMAIL = os.environ["SENDER_EMAIL"]
     SENDER_PASSWORD = os.environ["SENDER_PASSWORD"]
@@ -22,7 +19,7 @@ except KeyError as exc:
     raise KeyError from exc
 
 
-def transformar_csv_para_lista(nome_arquivo):
+def transformar_csv_para_lista(nome_arquivo: str):
     '''Transforma um arquivo csv em uma lista'''
     lista = []
 
@@ -36,16 +33,15 @@ def transformar_csv_para_lista(nome_arquivo):
     return lista
 
 
-def escrever_csv_da_lista(nome_arquivo, lista):
+def escrever_csv_da_lista(nome_arquivo: str, lista: list):
     '''Escreve um arquivo csv de uma lista'''
 
-    with open(nome_arquivo, 'w', encoding="UTF-8") as f:
-        # using csv.writer method from CSV package
-        write = csv.writer(f)
+    with open(nome_arquivo, 'w', encoding="UTF-8") as arquivo:
+        write = csv.writer(arquivo)
         write.writerow(lista)
 
 
-def verificar_estacao_offline(id_estacao):
+def verificar_estacao_offline(id_estacao: str):
     '''Checa se a estação meteorológica está offline pelo seu id'''
     url = API_URL.format(key=API_KEY, id=id_estacao)
     status_offline = False
@@ -74,10 +70,8 @@ def verificar_estacao_offline(id_estacao):
     return status_offline
 
 
-def enviar_email(lista_estacoes_offline, lista_estacoes_online):
-    '''Envia um email de alerta com a lista de estações especificada'''
-
-    subject = "🚨 Alerta de estações"
+def criar_corpo_email(lista_estacoes_offline: list, lista_estacoes_online: list):
+    '''Cria o corpo do email baseado nas listas de estações que mudaram de status'''
     body = ""
 
     if len(lista_estacoes_offline) > 0:
@@ -94,44 +88,58 @@ def enviar_email(lista_estacoes_offline, lista_estacoes_online):
             body += f"<li>{est}</li>"
         body += "</ul>"
 
+    return body
+
+
+def enviar_email(corpo_email: str, assunto: str):
+    '''Envia um email com o assunto e corpo especificados'''
+
     msg = email.message.Message()
-    msg["Subject"] = subject
+    msg["Subject"] = assunto
     msg["From"] = SENDER_EMAIL
     msg["To"] = RECEIVER_EMAIL
     msg.add_header("Content-Type", "text/html")
-    msg.set_payload(body)
+    msg.set_payload(corpo_email)
+    msg_encoded = msg.as_string().encode("utf-8")
 
     try:
         server = smtplib.SMTP('smtp.gmail.com', 587)
         server.starttls()
         server.login(msg["From"], SENDER_PASSWORD)
-        server.sendmail(msg["From"], msg["To"],
-                        msg.as_string().encode("utf-8"))
+        server.sendmail(msg["From"], msg["To"], msg_encoded)
         server.quit()
         logging.info("Email enviado aos destinatários")
     except Exception as err:
         logging.error("Erro ao enviar o email: %s", err)
 
 
-if __name__ == '__main__':
+def main():
+    '''Função principal a ser executada'''
+
     logging.info("Iniciando...")
     estacoes = transformar_csv_para_lista("estacoes.csv")
-    estacoes_offline_antes = transformar_csv_para_lista("estacoes_offline.csv")
+    offlines_antes = transformar_csv_para_lista("estacoes_offline.csv")
 
-    estacoes_offline_atual = []
+    offlines_atual = []
     for estacao in estacoes:
-        IS_OFFLINE = verificar_estacao_offline(estacao)
-        if IS_OFFLINE:
-            estacoes_offline_atual.append(estacao)
+        is_offline = verificar_estacao_offline(estacao)
+        if is_offline:
+            offlines_atual.append(estacao)
 
-    lista_ficaram_offline = list(
-        set(estacoes_offline_atual).difference(estacoes_offline_antes))
-    lista_ficaram_online = list(
-        set(estacoes_offline_antes).difference(estacoes_offline_atual))
+    ficaram_off = list(set(offlines_atual).difference(offlines_antes))
+    ficaram_on = list(set(offlines_antes).difference(offlines_atual))
 
-    if len(lista_ficaram_offline) > 0 or len(lista_ficaram_online) > 0:
-        enviar_email(lista_ficaram_offline, lista_ficaram_online)
+    if len(ficaram_off) > 0 or len(ficaram_on) > 0:
+        corpo_email = criar_corpo_email(ficaram_off, ficaram_on)
+        assunto_email = "🚨 Alerta de estações"
+        enviar_email(corpo_email, assunto_email)
 
-    escrever_csv_da_lista("estacoes_offline.csv", estacoes_offline_atual)
+    escrever_csv_da_lista("estacoes_offline.csv", offlines_atual)
 
     logging.info("Finalizando em %.2f s", (time.time() - START_TIME))
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, filename="programa.log",
+                        format="%(asctime)s - %(levelname)s - %(message)s")
+    main()
